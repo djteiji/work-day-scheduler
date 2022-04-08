8a3568a5c912b7a63ee75da1a94809cf8aef66d3
djteiji <djteiji@gmail.com>
2022-04-08 01:39:45 -0700

    add color changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
 $("#currentDay").text(moment().format('dddd, MMMM D, YYYY'));
+
+var saveButton = $(".saveBtn")
+
+
+var colorChange = function() {
+
+    var currentHour = moment().hour();
+
+    $(".time-block").each(function() {
+        var hour = parseInt($(this).attr("id"));
+        // console.log(hour);
+        
+        if (currentHour < hour) {
+            $(this).addClass("future");
+        }
+        else if (currentHour === hour) {
+            $(this).addClass("present");
+
+        } else if (currentHour > hour) {
+            $(this).addClass("past");
+        }
+    })
+};
+
+var storeEvent = function() {
+    
+}
+
+colorChange();
